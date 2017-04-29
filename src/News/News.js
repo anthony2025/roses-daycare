ed166e43af745f1b1417532469d211063104b459
Anthony Ascencio <anthony2025@gmail.com>
2017-04-29 14:54:34 -0400

    adds key prop to News

--- src/News/News.js
+++ src/News/News.js
@@ -12,7 +12,7 @@ export default class News extends Component {
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
     {
-      title: 'Making Homemade Clay',
+      title: 'Hopscotch Championship',
       image: 'img/4img.jpg',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
@@ -31,6 +31,7 @@ export default class News extends Component {
             title={article.title}
             image={article.image}
             text={article.text}
+            key={article.title}
           />
         ))}
         <div className={styles.link}>Our own blog will be coming soon.</div>
