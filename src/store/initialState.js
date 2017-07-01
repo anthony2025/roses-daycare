bbd881a7133d118158076386b51c3e533def36a3
Anthony Ascencio <anthony2025@gmail.com>
2017-07-01 04:38:40 -0400

    fixes typo in initialState causing bug

--- src/store/initialState.js
+++ src/store/initialState.js
@@ -1,62 +1,60 @@
 export default {
-  content: {
-    articles: {
-      '5e65': {
-        author: 'shirley',
-        date: 'May 26, 2017',
-        image: '/photos/clay.jpg',
-        text:
-          'With the help of a touch of flour and water, our kids had a blast playing with clay. The younger ones had a chance to stimulate their senses. We showed them to sink their little hands into the dough, watched them smile with the wetness, and let them have a chance to exercise their fine movement abilities. The older ones built it all, from snakes and hipos, to castles and frisbees. The only sad part was having to clean up afterwards!',
-        title: 'Making Homemade Clay'
-      },
-      f82a: {
-        author: 'carolina',
-        date: 'May 29, 2017',
-        image: '/photos/musical.jpg',
-        text:
-          'Lights, Camera, Action! With no choreography but a lot of swing, our kids played in their own musical. Ms. Fabiola narrated, and the kids took the chance to dress up and take improv acting. Everyone jumped enthusiastically to defeat the villain, and took turns making the weirdest faces they could come up with. But no one wanted to remain sit when the music came on between chapters.',
-        title: 'Little Tiny Musical'
-      }
+  articles: {
+    '5e65': {
+      author: 'shirley',
+      date: 'May 26, 2017',
+      image: '/photos/clay.jpg',
+      text:
+        'With the help of a touch of flour and water, our kids had a blast playing with clay. The younger ones had a chance to stimulate their senses. We showed them to sink their little hands into the dough, watched them smile with the wetness, and let them have a chance to exercise their fine movement abilities. The older ones built it all, from snakes and hipos, to castles and frisbees. The only sad part was having to clean up afterwards!',
+      title: 'Making Homemade Clay'
     },
-    contactInfo: {
-      address1: '6400 Franconia Rd',
-      address2: 'Springfield VA 22150',
-      email: 'rosesdaycare@gmail.com',
-      phone: '571 255 2105'
+    f82a: {
+      author: 'carolina',
+      date: 'May 29, 2017',
+      image: '/photos/musical.jpg',
+      text:
+        'Lights, Camera, Action! With no choreography but a lot of swing, our kids played in their own musical. Ms. Fabiola narrated, and the kids took the chance to dress up and take improv acting. Everyone jumped enthusiastically to defeat the villain, and took turns making the weirdest faces they could come up with. But no one wanted to remain sit when the music came on between chapters.',
+      title: 'Little Tiny Musical'
+    }
+  },
+  contactInfo: {
+    address1: '6400 Franconia Rd',
+    address2: 'Springfield VA 22150',
+    email: 'rosesdaycare@gmail.com',
+    phone: '571 255 2105'
+  },
+  intro: [
+    'Welcome! we are a family operated home day care in the Franconia area. For over 20 years we have make sure our kids can grow in a loving and understanding environment. We value early stimulation and play time, to show kids to be inquisitive and curious about the environment that surrounds them.',
+    'We follow the Funshine Curriculum™ and a myriad of science and exploration activities. Combined with lots of love, hugs and kindness, we make sure our kids minds are stimulated in all possible areas. From kinetic exercises and dancing; to science, color and crafts projects, we make sure kids are exposed in all the marvelous ways their creativity can blossom.'
+  ],
+  people: {
+    anthony: {
+      description:
+        'Anthony is our marketing wizard, you will find him around the blog and facebook page, keeping parents up to date on what we are up to each week.',
+      image: 'avatars/anthony.jpg',
+      name: 'Anthony Ascencio',
+      subtitle: 'Marketing'
     },
-    intro: [
-      'Welcome! we are a family operated home day care in the Franconia area. For over 20 years we have make sure our kids can grow in a loving and understanding environment. We value early stimulation and play time, to show kids to be inquisitive and curious about the environment that surrounds them.',
-      'We follow the Funshine Curriculum™ and a myriad of science and exploration activities. Combined with lots of love, hugs and kindness, we make sure our kids minds are stimulated in all possible areas. From kinetic exercises and dancing; to science, color and crafts projects, we make sure kids are exposed in all the marvelous ways their creativity can blossom.'
-    ],
-    people: {
-      anthony: {
-        description:
-          'Anthony is our marketing wizard, you will find him around the blog and facebook page, keeping parents up to date on what we are up to each week.',
-        image: 'avatars/anthony.jpg',
-        name: 'Anthony Ascencio',
-        subtitle: 'Marketing'
-      },
-      carolina: {
-        description:
-          'Ms. Carolina has been our most beloved teacher for the last five years. She teaches kids to share and empathize with one another, and shower each one of them with infinite affection.',
-        image: 'avatars/carolina.jpg',
-        name: 'Carolina Bau',
-        subtitle: 'Teacher'
-      },
-      fabiola: {
-        description:
-          'Ms. Fabiola has an infinite creativity to come up with new crafts and activities. She is a loving mother of two girls, and a natural born leader.',
-        image: 'avatars/fabiola.jpg',
-        name: 'Fabiola Pacheco',
-        subtitle: 'Lead Teacher'
-      },
-      shirley: {
-        description:
-          "Ms. Shirley is Rose's daughter and our current Principal. She holds a B.S. in Business Administration, 7 years in experience in child care at multiple centers, and an unending love for kids and her own children.",
-        image: 'avatars/shirley.jpg',
-        name: 'Shirley Sanchez',
-        subtitle: 'Principal'
-      }
+    carolina: {
+      description:
+        'Ms. Carolina has been our most beloved teacher for the last five years. She teaches kids to share and empathize with one another, and shower each one of them with infinite affection.',
+      image: 'avatars/carolina.jpg',
+      name: 'Carolina Bau',
+      subtitle: 'Teacher'
+    },
+    fabiola: {
+      description:
+        'Ms. Fabiola has an infinite creativity to come up with new crafts and activities. She is a loving mother of two girls, and a natural born leader.',
+      image: 'avatars/fabiola.jpg',
+      name: 'Fabiola Pacheco',
+      subtitle: 'Lead Teacher'
+    },
+    shirley: {
+      description:
+        "Ms. Shirley is Rose's daughter and our current Principal. She holds a B.S. in Business Administration, 7 years in experience in child care at multiple centers, and an unending love for kids and her own children.",
+      image: 'avatars/shirley.jpg',
+      name: 'Shirley Sanchez',
+      subtitle: 'Principal'
     }
   }
 }
