e7686acd973142d1799aa132627b3f5d16fbdead
Anthony Ascencio <anthony2025@gmail.com>
2017-08-08 01:48:49 -0400

    add map in Contact page

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -2,14 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 
 import appear from 'styling/appearAnimation'
+import Map from './Map'
 import Form from './Form'
 import Info from './Info'
 
 export default function Contact(props) {
   return (
     <Wrapper className={props.className}>
-      <StyledInfo />
-      <StyledForm />
+      <Container>
+        <StyledInfo />
+        <StyledForm />
+      </Container>
+      <StyledMap />
     </Wrapper>
   )
 }
@@ -18,20 +22,29 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  animation: ${appear} .9s ease;
+`
 
+const Container = styled.div`
+  width: 100%;
   @media (max-width: 500px) {
     font-size: 15px;
     padding: 40px 10%;
   }
-  @media (min-width: 500px) and (max-width: 950px) {
+  @media (min-width: 500px) and (max-width: 1000px) {
     font-size: 19px;
     padding: 40px 20%;
   }
-  @media (min-width: 950px) {
+  @media (min-width: 1000px) {
     font-size: 20px;
-    padding: 40px 25%;
+    padding: 40px 30%;
   }
-  animation: ${appear} .9s ease;
+`
+
+const StyledMap = styled(Map)`
+  margin-top: 30px;
+  width: 100%;
+  height: 400px;
 `
 
 const StyledInfo = styled(Info)`
---
+++ src/containers/Contact/Map/Map.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import GoogleMap from 'google-map-react'
+
+Map.propTypes = {
+  // apiKey: PropTypes.string.isRequired,
+  center: PropTypes.shape({
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired
+  }).isRequired,
+  zoom: PropTypes.number.isRequired
+}
+
+Map.defaultProps = {
+  // apiKey: process.env.REACT_APP_GAPI_KEY,
+  center: {
+    lat: 38.7781517,
+    lng: -77.160154
+  },
+  zoom: 17
+}
+
+export default function Map(props) {
+  return (
+    <div className={props.className}>
+      <GoogleMap
+        defaultCenter={props.center}
+        defaultZoom={props.zoom}
+        // bootstrapURLKeys={{key: this.props.apiKey}}
+      />
+    </div>
+  )
+}
